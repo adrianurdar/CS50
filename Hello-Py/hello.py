80afad6a9201b0e098489c311deaaa5f625f0949
Adrian Urdar <47056570+adrianurdar@users.noreply.github.com>
2020-11-18 13:56:39 +0200

    Create hello.py

---
+++ Hello-Py/hello.py
@@ -0,0 +1,11 @@
+# Hello World program
+# CS50 2019
+
+# Import string from CS50 library
+from cs50 import get_string
+
+# Prompt the user for name
+s = get_string("What is your name?\n")
+
+# Print hello + user input
+print("hello, ", s)
